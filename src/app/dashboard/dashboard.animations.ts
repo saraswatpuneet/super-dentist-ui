99c2f75e65f4fedaf767677ecae0e62cec01aac4
Daniel Tupa <xthecounsel@gmail.com>
2020-10-24 21:44:39 -0500

    Update sidenav so it collapses and highs nav items

---
+++ src/app/dashboard/dashboard.animations.ts
@@ -0,0 +1,13 @@
+import { trigger, transition, style, animate, state } from '@angular/animations';
+
+export const dashboardAnimations = [
+  trigger('sidebarToggle', [
+    state('*', style({
+      opacity: 1
+    })),
+    state('void', style({
+      opacity: 0
+    })),
+    transition('* <=> void', animate('100ms ease-in'))
+  ]),
+];
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,20 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { DialogService } from '../shared/dialog/dialog.service';
 
+import { dashboardAnimations } from './dashboard.animations';
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
+  animations: dashboardAnimations
 })
 export class DashboardComponent implements OnInit {
+  expanded = true;
+  expandedKey = 'sdNavExpanded';
 
   constructor(private dialogService: DialogService) { }
 
   ngOnInit(): void {
+    const expanded = localStorage.getItem(this.expandedKey);
+    if (expanded === 'false') {
+      this.expanded = false;
+    }
   }
 
   openCreateReferral(): void {
     this.dialogService.openCreateReferral();
+  }
+
+  toggleNav(): void {
+    this.expanded = !this.expanded;
+    localStorage.setItem(this.expandedKey, `${this.expanded}`);
   }
 
 }
