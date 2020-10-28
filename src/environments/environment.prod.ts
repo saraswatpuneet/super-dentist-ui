d4e139fbafd4ac7e9619bc60b307a3cbf05f7758
Puneet Saraswat <puneet.saraswat@theagilehub.net>
2020-10-27 20:44:21 -0500

    update for env variable

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,8 @@
 
-const env = require('env-var');
-
 export const environment = {
   production: true,
   firebase: {
-    apiKey: env.get('SUPER_DENTIST_KEY'),
+    apiKey: process.env.SUPER_DENTIST_KEY,
     authDomain: 'superdentist.firebaseapp.com',
   }
 };
