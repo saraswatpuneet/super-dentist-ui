16118763b71de1cfb2c32e2242e884eddb18e5ad
Puneet Saraswat <puneet.saraswat@theagilehub.net>
2020-10-27 20:52:37 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: process.env.SUPER_DENTIST_KEY,
+    apiKey: 'SUPER_DENTIST_KEY', // TODO get the key from env
     authDomain: 'superdentist.firebaseapp.com',
   }
 };
