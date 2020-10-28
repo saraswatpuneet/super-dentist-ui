ad9eb24cd2d50b8b336f0b61f0d1c095e7c47a62
Daniel Tupa <xthecounsel@gmail.com>
2020-10-27 20:39:45 -0500

    Add authorization to each request and register API

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { DialogService } from '../shared/dialog/dialog.service';
 
 import { dashboardAnimations } from './dashboard.animations';
+import { ClinicService } from 'src/app/shared/services/clinic.service';
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-dashboard',
@@ -11,11 +13,23 @@ import { dashboardAnimations } from './dashboard.animations';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor(private dialogService: DialogService) { }
+  constructor(
+    private dialogService: DialogService,
+    private clinicService: ClinicService
+  ) { }
 
   ngOnInit(): void { }
 
   openCreateReferral(): void {
     this.dialogService.openCreateReferral();
+  }
+
+  getAllDoctors(): void {
+    console.log('click');
+    this.clinicService.getAllDoctors().pipe(take(1)).subscribe(console.log);
+  }
+
+  getClinics(): void {
+    this.clinicService.getClinics().pipe(take(1)).subscribe(console.log);
   }
 }
