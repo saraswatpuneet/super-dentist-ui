83c7b4a62a2bc942afd215090a1e74c1944542e0
Daniel Tupa <xthecounsel@gmail.com>
2020-10-29 00:04:06 -0500

    test config replace env variables

---
+++ config.ts
@@ -0,0 +1,18 @@
+import { writeFile } from 'fs';
+
+const targetPath = './src/environments/environment.prod.ts';
+
+const envConfigFile = `export const environment = {
+   production: true,
+   firebase: {
+        apiKey: '${process.env.SUPER_DENTIST_KEY}',
+        authDomain: 'superdentist.firebaseapp.com'
+    },
+};
+`;
+
+writeFile(targetPath, envConfigFile, 'utf8', (err) => {
+  if (err) {
+    return console.log(err);
+  }
+});
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,7 @@
-
-const env = require('env-var');
-
 export const environment = {
-  production: true,
-  firebase: {
-    apiKey: env.get('SUPER_DENTIST_KEY'),
-    authDomain: 'superdentist.firebaseapp.com',
-  }
+   production: true,
+   firebase: {
+        apiKey: 'undefined',
+        authDomain: 'superdentist.firebaseapp.com'
+    },
 };
