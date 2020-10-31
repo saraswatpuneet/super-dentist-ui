4596450298f2ccc73acf427b8ae5278a8802097b
Daniel Tupa <xthecounsel@gmail.com>
2020-10-31 01:29:13 -0500

    Add referrals and specialists

--- src/app/referrals/referrals.module.ts
+++ src/app/referrals/referrals.module.ts
@@ -3,12 +3,16 @@ import { CommonModule } from '@angular/common';
 
 import { ReferralsRoutingModule } from './referrals-routing.module';
 import { ReferralsComponent } from './referrals.component';
+import { MatIconModule } from '@angular/material/icon';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 
 @NgModule({
   declarations: [ReferralsComponent],
   imports: [
     CommonModule,
+    FlexLayoutModule,
+    MatIconModule,
     ReferralsRoutingModule
   ]
 })
--- src/app/specialist/specialist.module.ts
+++ src/app/specialist/specialist.module.ts
@@ -3,12 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { SpecialistRoutingModule } from './specialist-routing.module';
 import { SpecialistComponent } from './specialist.component';
-
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [SpecialistComponent],
   imports: [
     CommonModule,
+    FlexLayoutModule,
+    MatIconModule,
     SpecialistRoutingModule
   ]
 })
