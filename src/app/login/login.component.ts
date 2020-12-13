ab19d978be7eb81fc7aad0c10c3500889431bc4a
Puneet Saraswat <puneet.saraswat@theagilehub.net>
2020-12-12 23:39:43 -0600

    set persistence state

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,6 +6,7 @@ import { catchError, take } from 'rxjs/operators';
 import { Base } from 'src/app/shared/base/base-component';
 import { Router } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { auth } from 'firebase/app';
 
 @Component({
   selector: 'app-login',
@@ -19,9 +20,12 @@ export class LoginComponent extends Base implements OnInit {
 
   constructor(
     private fb: FormBuilder,
-    private auth: AngularFireAuth,
+    private fauth: AngularFireAuth,
     private router: Router
-  ) { super(); }
+  ) {
+    super(); 
+    fauth.setPersistence(auth.Auth.Persistence.SESSION)
+  }
 
   ngOnInit(): void {
     this.formGroup = this.fb.group({
@@ -41,7 +45,7 @@ export class LoginComponent extends Base implements OnInit {
     this.loading = true;
     this.errorMessage = '';
     const { email, password } = this.formGroup.value;
-    from(this.auth.signInWithEmailAndPassword(email, password)).pipe(
+    from(this.fauth.signInWithEmailAndPassword(email, password)).pipe(
       catchError(err => {
         this.errorMessage = err.message;
         return of(null);
