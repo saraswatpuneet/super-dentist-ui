0a2da4e140009d8754e67ec5317a6f2d16b8f343
DTwoPaw <xthecounsel@gmail.com>
2020-12-14 13:32:17 -0600

    Merge

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -6,6 +6,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 import { LoginComponent } from './login.component';
 import { LoginRoutingModule } from './login-routing.module';
@@ -19,6 +20,7 @@ import { LoginRoutingModule } from './login-routing.module';
     ReactiveFormsModule,
     MatIconModule,
     MatProgressBarModule,
+    MatCardModule,
     MatButtonModule,
     MatInputModule,
     LoginRoutingModule
