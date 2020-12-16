7efcbcb2557d61a75b1ec0f3fc106570bdd1648e
DTwoPaw <xthecounsel@gmail.com>
2020-12-15 22:15:33 -0600

    Displace css card and allow theme toggle

--- src/app/verify/verify.module.ts
+++ src/app/verify/verify.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 import { VerifyRoutingModule } from './verify-routing.module';
 import { VerifyComponent } from './verify.component';
@@ -12,6 +13,7 @@ import { VerifyComponent } from './verify.component';
   imports: [
     CommonModule,
     MatIconModule,
+    MatCardModule,
     MatButtonModule,
     FlexLayoutModule,
     VerifyRoutingModule
