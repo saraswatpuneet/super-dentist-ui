809b5fd87458871ee410bfd97c4fef89a8bdf24e
DTwoPaw <xthecounsel@gmail.com>
2020-12-15 21:59:46 -0600

    Update verify to use ngrx instead of promise chain/ngrx combo

--- src/app/verify/verify.component.ts
+++ src/app/verify/verify.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { Router } from '@angular/router';
+import { take, flatMap } from 'rxjs/operators';
+import { from } from 'rxjs';
+
 import { ClinicService } from '../shared/services/clinic.service';
-import { switchMap, take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-verify',
@@ -13,21 +15,20 @@ export class VerifyComponent implements OnInit {
   sentEmail = false;
   userEmail = '';
 
-  constructor(private auth: AngularFireAuth, private router: Router, private clinicService: ClinicService,) { }
+  constructor(private auth: AngularFireAuth, private router: Router, private clinicService: ClinicService, ) { }
 
   ngOnInit(): void {
     this.auth.currentUser.then(user => this.userEmail = user.email);
   }
 
   resendEmail(): void {
-    
-    this.auth.currentUser.then(user => {
-      this.clinicService.registerAdmin(user.email, true).pipe(take(1))
-      .subscribe(response => {
-        if (response.error == null) {
-          this.sentEmail = true;
-        }
-      });
+    from(this.auth.currentUser).pipe(
+      flatMap(user => this.clinicService.registerAdmin(user.email, true)),
+      take(1)
+    ).subscribe(res => {
+      if (res.error == null) {
+        this.sentEmail = true;
+      }
     });
   }
 
