74c1b26f24aca6a3b8771496531e9ba1b017d872
DTwoPaw <xthecounsel@gmail.com>
2020-12-15 22:53:44 -0600

    Setup for specialist type mapping and reasons

--- src/app/shared/services/clinic.ts
+++ src/app/shared/services/clinic.ts
@@ -24,3 +24,20 @@ export interface ClinicServicesOffered {
   serviceGroup: string;
   serviceList: string[];
 }
+
+// Key types for specialist
+export type SpecialistType = 'endodontics' | 'oralSurgry' | 'periodontics' | 'pediatricDentist' | 'prosthodontics';
+
+export function specialistReasons(): { [key: string]: any } {
+  return {
+    endodontics: {},
+    oralSurgry: {},
+    periodontics: {},
+    pediatricDentist: {},
+    prosthodontics: {},
+  };
+}
+
+export function sortSpecialistReasons(): string[] {
+  return ['endodontics', 'oralSurgry', 'periodontics', 'pediatricDentist', 'prosthodontics'];
+}
