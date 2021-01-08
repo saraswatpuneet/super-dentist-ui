46b6e433ac09bc130dcd28e78e22db911d64151a
DTwoPaw <xthecounsel@gmail.com>
2021-01-07 19:11:45 -0600

    Upgrade to angular 11

--- src/app/referrals-beta/referrals-beta.component.spec.ts
+++ src/app/referrals-beta/referrals-beta.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ReferralsBetaComponent } from './referrals-beta.component';
 
@@ -6,7 +6,7 @@ describe('ReferralsBetaComponent', () => {
   let component: ReferralsBetaComponent;
   let fixture: ComponentFixture<ReferralsBetaComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ReferralsBetaComponent ]
     })
--- src/app/specialist/shared/create-referral/create-referral.component.spec.ts
+++ src/app/specialist/shared/create-referral/create-referral.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { CreateReferralComponent } from './create-referral.component';
 
@@ -6,7 +6,7 @@ describe('CreateReferralComponent', () => {
   let component: CreateReferralComponent;
   let fixture: ComponentFixture<CreateReferralComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ CreateReferralComponent ]
     })
