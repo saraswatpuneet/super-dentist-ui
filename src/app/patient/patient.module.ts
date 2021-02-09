81cbea5a7e3c8e44718ea2e7df95ad8b5ed693dd
DTwoPaw <xthecounsel@gmail.com>
2021-02-09 00:30:55 -0600

    Hook google maps api into angular

--- src/app/patient/patient.module.ts
+++ src/app/patient/patient.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatCardModule } from '@angular/material/card';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatRadioModule } from '@angular/material/radio';
 import { NgxMaskModule } from 'ngx-mask';
 
 import { PatientRoutingModule } from './patient-routing.module';
@@ -15,10 +16,12 @@ import { PatientComponent } from './patient.component';
   declarations: [PatientComponent],
   imports: [
     CommonModule,
+    FormsModule,
     ReactiveFormsModule,
     FlexLayoutModule,
     NgxMaskModule,
     MatButtonModule,
+    MatRadioModule,
     MatProgressSpinnerModule,
     MatCardModule,
     MatIconModule,
