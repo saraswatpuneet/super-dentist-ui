f242545324742f58eca139765d3798539f664946
DTwoPaw <xthecounsel@gmail.com>
2021-02-14 13:35:40 -0600

    change to prod for now

---
+++ src/environments/environment.local.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCp-tO9Rk5iWTeg-bqtP2tvFaW9dXlsS6k',
+    authDomain: 'superdentist.firebaseapp.com',
+  },
+  baseUrl: 'https://dev.superdentist.io/api/sd/v1',
+  cloudFunctionUrl: 'https://us-central1-superdentist.cloudfunctions.net',
+  cloudFunctionQRReferralUrl: 'https://us-central1-superdentist.cloudfunctions.ne/sd-qr-referral-dev',
+};
