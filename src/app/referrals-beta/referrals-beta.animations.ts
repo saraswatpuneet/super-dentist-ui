62b7fbd3c632e0cd96c854bdeedb9eb061595332
DTwoPaw <xthecounsel@gmail.com>
2021-02-18 16:09:34 -0600

    Color changes

--- src/app/referrals-beta/referrals-beta.animations.ts
+++ src/app/referrals-beta/referrals-beta.animations.ts
@@ -10,5 +10,15 @@ export const referralsBetaAnimations = [
       style({ width: '375px', opacity: 1 }),
       animate('250ms', style({ width: 0, opacity: 0 }))
     ]),
+  ]),
+  trigger('accordion', [
+    transition(':enter', [
+      style({ height: 0 }),
+      animate('250ms', style({ width: '375px', opacity: 1 }))
+    ]),
+    transition(':leave', [
+      style({ height: 'auto' }),
+      animate('250ms', style({ width: 0, opacity: 0 }))
+    ]),
   ])
 ];
