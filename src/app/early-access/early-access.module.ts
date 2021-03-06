e51f89dfa5d1c1e81f395537a887423e593d48bf
DTwoPaw <xthecounsel@gmail.com>
2021-03-06 13:41:39 -0600

    Submit requst for access form

--- src/app/early-access/early-access.module.ts
+++ src/app/early-access/early-access.module.ts
@@ -1,14 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { NgxMaskModule } from 'ngx-mask';
 
 import { EarlyAccessRoutingModule } from './early-access-routing.module';
 import { EarlyAccessComponent } from './early-access.component';
-
 
 @NgModule({
   declarations: [EarlyAccessComponent],
   imports: [
     CommonModule,
+    FlexLayoutModule,
+    MatListModule,
+    NgxMaskModule,
+    MatCardModule,
+    MatIconModule,
+    MatButtonModule,
     EarlyAccessRoutingModule
   ]
 })
