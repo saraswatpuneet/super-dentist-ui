57fa3308a98364c95ceb9d855612485b1a1248e1
DTwoPaw <xthecounsel@gmail.com>
2021-03-09 20:15:32 -0600

    Add subscriber title and format DOB month

--- src/app/secure/insurance/insurance.component.ts
+++ src/app/secure/insurance/insurance.component.ts
@@ -65,6 +65,20 @@ export class InsuranceComponent implements OnInit, OnDestroy {
   insurances = [];
   selectedDental = false;
   selectedMedical = false;
+  displayMonths = {
+    1: 'January',
+    2: 'Febuary',
+    3: 'March',
+    4: 'April',
+    5: 'May',
+    6: 'June',
+    7: 'July',
+    8: 'August',
+    9: 'September',
+    10: 'October',
+    11: 'November',
+    12: 'December',
+  };
 
   constructor(
     private fb: FormBuilder,
