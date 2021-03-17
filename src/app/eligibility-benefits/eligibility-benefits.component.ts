2fe0070790ee3e3526c1f6a56fa669e97c8828bf
DTwoPaw <xthecounsel@gmail.com>
2021-03-17 08:31:35 -0500

    Move eligibility to insurance completion it was in the wrong spot

--- src/app/eligibility-benefits/eligibility-benefits.component.ts
+++ src/app/eligibility-benefits/eligibility-benefits.component.ts
@@ -1,63 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { Base } from '../shared/base/base-component';
-import { takeUntil, map, filter, switchMap, distinctUntilChanged } from 'rxjs/operators';
-import { Subject } from 'rxjs';
-
-import { ClinicService } from '../shared/services/clinic.service';
-import { PatientService } from '../shared/services/patient.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-eligibility-benefits',
   templateUrl: './eligibility-benefits.component.html',
   styleUrls: ['./eligibility-benefits.component.scss']
 })
-export class EligibilityBenefitsComponent extends Base implements OnInit {
-  clinics = [];
-  selectedClinicAddressId = '';
-  patients = [];
+export class EligibilityBenefitsComponent {
 
-  private patientSubject = new Subject<string>();
-
-  constructor(
-    private clinicService: ClinicService,
-    private patientService: PatientService,
-  ) {
-    super();
-  }
-
-  ngOnInit(): void {
-    this.watchPatients();
-    this.clinicService.getAllClinics()
-      .pipe(
-        filter(r => !!r),
-        map(res => res.data),
-        takeUntil(this.unsubscribe$))
-      .subscribe(clinics => {
-        this.clinics = clinics;
-        console.log(clinics);
-        if (clinics && clinics.length > 0) {
-          this.patientSubject.next(clinics[0].addressId);
-        }
-      });
-  }
-
-  getPatients(addressId: string): void {
-    console.log('addressId for request', addressId);
-    this.patientSubject.next(addressId);
-  }
-
-  private watchPatients(): void {
-    this.patientSubject.pipe(
-      // distinctUntilChanged(),
-      switchMap(addressId => {
-        this.selectedClinicAddressId = addressId;
-        return this.patientService.getAllPatientsForClinic(addressId);
-      }),
-      map(res => res.data),
-      takeUntil(this.unsubscribe$)
-    )
-      .subscribe(res => {
-        console.log('Patients for clinic', res);
-      });
-  }
+  constructor() { }
 }
--- src/app/insurance-completion/insurance-completion.component.ts
+++ src/app/insurance-completion/insurance-completion.component.ts
@@ -1,18 +1,64 @@
 import { Component, OnInit } from '@angular/core';
 
 import { InsuranceGroup, InsuranceMap, generateGroups, generateGeneralInformation, generateCategories } from './insurance-completion';
+import { Subject } from 'rxjs';
+import { Base } from '../shared/base/base-component';
+import { PatientService } from '../shared/services/patient.service';
+import { ClinicService } from '../shared/services/clinic.service';
+import { filter, map, takeUntil, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-insurance-completion',
   templateUrl: './insurance-completion.component.html',
   styleUrls: ['./insurance-completion.component.scss']
 })
-export class InsuranceCompletionComponent implements OnInit {
+export class InsuranceCompletionComponent extends Base implements OnInit {
   groups: InsuranceGroup[] = [];
+  clinics = [];
+  selectedClinicAddressId = '';
+  patients = [];
 
-  constructor() { }
+  private patientSubject = new Subject<string>();
+
+  constructor(
+    private clinicService: ClinicService,
+    private patientService: PatientService,
+  ) { super(); }
 
   ngOnInit(): void {
     this.groups = generateGroups();
+    this.watchPatients();
+    this.clinicService.getAllClinics()
+      .pipe(
+        filter(r => !!r),
+        map(res => res.data),
+        takeUntil(this.unsubscribe$))
+      .subscribe(clinics => {
+        this.clinics = clinics;
+        console.log(clinics);
+        if (clinics && clinics.length > 0) {
+          this.patientSubject.next(clinics[0].addressId);
+        }
+      });
+  }
+
+  getPatients(addressId: string): void {
+    console.log('addressId for request', addressId);
+    this.patientSubject.next(addressId);
+  }
+
+  private watchPatients(): void {
+    this.patientSubject.pipe(
+      // distinctUntilChanged(),
+      switchMap(addressId => {
+        this.selectedClinicAddressId = addressId;
+        return this.patientService.getAllPatientsForClinic(addressId);
+      }),
+      map(res => res.data),
+      takeUntil(this.unsubscribe$)
+    )
+      .subscribe(res => {
+        console.log('Patients for clinic', res);
+      });
   }
 }
