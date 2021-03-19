1d7628db81f3d14ebb40b88915d529a20dc513dc
DTwoPaw <xthecounsel@gmail.com>
2021-03-19 09:23:39 -0500

    Add success confirm button after submit referral

--- src/app/shared/insurance-registration/insurance-registration.component.ts
+++ src/app/shared/insurance-registration/insurance-registration.component.ts
@@ -134,9 +134,6 @@ export class InsuranceRegistrationComponent implements OnInit {
       .pipe(take(1))
       .subscribe((res) => {
         this.state = PatientStates.Success;
-        if (!this.referralId) {
-          this.cancel();
-        }
       });
   }
 
