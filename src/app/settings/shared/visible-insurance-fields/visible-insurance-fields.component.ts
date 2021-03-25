f53c4293a606940d38b1ea708728b358bdfb41a5
DTwoPaw <xthecounsel@gmail.com>
2021-03-24 21:57:29 -0500

    Show saved records for dental codes

--- src/app/settings/shared/visible-insurance-fields/visible-insurance-fields.component.ts
+++ src/app/settings/shared/visible-insurance-fields/visible-insurance-fields.component.ts
@@ -1,9 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { takeUntil, map, switchMap, take } from 'rxjs/operators';
-import { Subject } from 'rxjs';
+import { takeUntil, map, switchMap, take, catchError } from 'rxjs/operators';
+import { Subject, of, forkJoin } from 'rxjs';
 
 import { ClinicService } from 'src/app/shared/services/clinic.service';
-import { PatientService } from 'src/app/shared/services/patient.service';
 import { Base } from 'src/app/shared/base/base-component';
 import { InsuranceService } from 'src/app/shared/services/insurance.service';
 import { DentalBreakDowns, DentalInsuranceKeys } from 'src/app/shared/services/insurance';
@@ -17,37 +16,30 @@ export class VisibleInsuranceFieldsComponent extends Base implements OnInit {
   clinics: any;
   selectedClinic: any;
   insuranceCodes: DentalBreakDowns;
-  selectedCodes: DentalBreakDowns = {
-    key: '',
-    label: '',
-    breakDownKeys: [],
-    breakDowns: {}
-  };
+  ids = {};
   private triggerInsurance = new Subject();
-  private codeMap = {};
+  codeMap = {};
 
   constructor(
     private clinicService: ClinicService,
-    private patientService: PatientService,
     private insuranceService: InsuranceService
   ) { super(); }
 
   ngOnInit(): void {
     this.watchTrigger();
-    this.insuranceService.getPracticeCodes()
-      .pipe(take(1))
-      .subscribe(codes => this.insuranceCodes = codes);
 
-    this.clinicService.getClinics()
-      .pipe(
-        map(res => res.data.clinicDetails),
-        takeUntil(this.unsubscribe$)
-      )
-      .subscribe(clinics => {
-        this.clinics = clinics;
-        this.selectedClinic = this.clinics[0];
-        this.triggerInsurance.next();
-      });
+    forkJoin([
+      this.insuranceService.getPracticeCodes().pipe(take(1)),
+      this.clinicService.getClinics().pipe(map(res => res.data.clinicDetails), take(1)),
+    ]).pipe(
+      takeUntil(this.unsubscribe$)
+    ).subscribe(([codes, clinics]) => {
+      this.insuranceCodes = codes;
+      this.clinics = clinics;
+      this.selectedClinic = this.clinics[0];
+      this.triggerInsurance.next();
+    });
+
   }
 
   submitCodes(): void {
@@ -63,45 +55,38 @@ export class VisibleInsuranceFieldsComponent extends Base implements OnInit {
       });
     });
 
-    console.log(keys);
     this.clinicService.saveSelectedPracticeCodes(this.selectedClinic.addressId, keys).pipe(take(1)).subscribe(console.log);
-
-
-    // this.selectedCodes.breakDownKeys = Object.keys(this.selectedCodes.breakDowns);
-    // this.selectedCodes.breakDownKeys.forEach(k => {
-    //   this.selectedCodes.breakDowns[k].breakDownKeys = Object.keys(this.selectedCodes.breakDowns[k].breakDowns);
-    // });
-    // this.selectedCodes.key = this.insuranceCodes.key;
-    // this.selectedCodes.label = this.insuranceCodes.label;
-    // this.clinicService.saveSelectedPracticeCodes(this.selectedClinic.addressId, this.selectedCodes).pipe(take(1)).subscribe(console.log);
-    // console.log('Selected Clinic', this.selectedClinic);
-    // console.log('Selected Codes', this.selectedCodes);
   }
 
   toggleBreakDown(checked: boolean, key: string, keyIndex: number, subKey: string, subKeyIndex: number): void {
     if (checked) {
+      this.ids[subKey] = true;
       if (!this.codeMap[keyIndex]) {
         this.codeMap[keyIndex] = {};
       }
       this.codeMap[keyIndex][subKeyIndex] = true;
     } else {
+      delete this.ids[subKey];
       delete this.codeMap[keyIndex][subKeyIndex];
     }
-
-    // if (checked) {
-    //   if (!this.selectedCodes.breakDowns[breakDown.key]) {
-    //     this.selectedCodes.breakDowns[breakDown.key] = { key: breakDown.key, label: breakDown.label, breakDowns: {} };
-    //   }
-    //   this.selectedCodes.breakDowns[breakDown.key].breakDowns[subBreakDown.key] = subBreakDown;
-    // } else {
-    //   delete this.selectedCodes.breakDowns[breakDown.key].breakDowns[subBreakDown.key];
-    // }
   }
 
   private watchTrigger(): void {
     this.triggerInsurance.pipe(
-      switchMap(() => this.clinicService.getSelectedPracticeCodes(this.selectedClinic.addressId)),
+      switchMap(() =>
+        this.clinicService.getSelectedPracticeCodes(this.selectedClinic.addressId)
+          .pipe(map(r => r.data), catchError(() => of([])))
+      ),
       takeUntil(this.unsubscribe$)
-    ).subscribe(console.log);
+    ).subscribe((res) => {
+      res.forEach(group => {
+        const groupIndex = this.insuranceCodes.breakDownKeys.indexOf(group.groupId);
+        this.codeMap[groupIndex] = {};
+        group.codeIds.forEach(id => {
+          this.ids[id] = true;
+          this.codeMap[groupIndex][this.insuranceCodes.breakDowns[group.groupId].breakDownKeys.indexOf(id)] = true;
+        });
+      });
+    });
   }
 }
