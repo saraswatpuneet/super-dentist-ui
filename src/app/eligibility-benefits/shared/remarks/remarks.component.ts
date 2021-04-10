239af76bc1c34e35dd44e752106ce25a0ce4f0f1
DTwoPaw <xthecounsel@gmail.com>
2021-04-10 08:52:45 -0500

    type cast remarks to any

--- src/app/eligibility-benefits/shared/remarks/remarks.component.ts
+++ src/app/eligibility-benefits/shared/remarks/remarks.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./remarks.component.scss']
 })
 export class RemarksComponent implements OnInit {
-  @Input() remarks = {};
+  @Input() remarks: any = {};
 
   constructor() { }
 
