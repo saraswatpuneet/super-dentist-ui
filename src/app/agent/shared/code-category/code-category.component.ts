b13747253dbc836a01b8ee3ad21a94ee2c4c954f
DTwoPaw <xthecounsel@gmail.com>
2021-04-19 23:30:03 -0400

    Clear values on min / range values

--- src/app/agent/shared/code-category/code-category.component.ts
+++ src/app/agent/shared/code-category/code-category.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { DentalBreakDowns } from 'src/app/shared/services/insurance';
-import { FormArray, FormGroup } from '@angular/forms';
+import { FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-code-category',
@@ -20,40 +20,18 @@ export class CodeCategoryComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  checkOtherOptions($event, selectThatChanged: string): void {
-    // console.log($event, selectThatChanged, this.groups);
-
-    // removes key from all codes
-    this.codeList.forEach(key => {
-      this.groups.controls.forEach((group: FormGroup) => {
-        const control = group.controls.codes.get(key);
-        if (control) {
-          const cas = control.get('sharedCodes');
-          if (cas.value) {
-            const index = cas.value.indexOf(selectThatChanged);
-            if (index > -1) {
-              cas.value.splice(index, 1);
-              cas.setValue([...cas.value]);
-            }
-          }
-        }
-      });
+  changeFixed(index: number, breakdownKey: string): void {
+    const control = this.groups.controls[index].get(breakdownKey);
+    control.patchValue({
+      min: undefined,
+      max: undefined
     });
+  }
 
-    //  Adds the key to other codes that were selected
-    $event.value.forEach(key => {
-      this.groups.controls.forEach((group: FormGroup) => {
-        const control = group.controls.codes.get(key);
-        if (control) {
-          const cas = control.get('sharedCodes');
-          if (cas.value) {
-            cas.setValue([selectThatChanged, ...cas.value]);
-          } else {
-            cas.setValue([selectThatChanged]);
-          }
-        }
-      });
+  changeRange(index: number, breakdownKey: string): void {
+    const control = this.groups.controls[index].get(breakdownKey);
+    control.patchValue({
+      fixed: undefined
     });
-
   }
 }
