ff2374dca248fc482d23d1ba4d74d291a96718c9
DTwoPaw <xthecounsel@gmail.com>
2021-05-03 17:26:19 -0500

    Add get codes

--- src/app/agent/dental-insurance/dental-insurance.component.ts
+++ src/app/agent/dental-insurance/dental-insurance.component.ts
@@ -6,6 +6,8 @@ import { forkJoin } from 'rxjs';
 import { ClinicService } from 'src/app/shared/services/clinic.service';
 import { PatientService } from 'src/app/shared/services/patient.service';
 import { Base } from 'src/app/shared/base/base-component';
+import { DentalBreakDowns } from 'src/app/shared/services/insurance';
+import { InsuranceService } from 'src/app/shared/services/insurance.service';
 
 @Component({
   selector: 'app-dental-insurance',
@@ -17,12 +19,14 @@ export class DentalInsuranceComponent extends Base implements OnInit {
   patientId = '';
   clinic: any;
   patient: any;
+  savedCodes: DentalBreakDowns = this.newSavedCodes();
 
   constructor(
     private router: Router,
     private route: ActivatedRoute,
     private clinicService: ClinicService,
     private patientService: PatientService,
+    private insuranceService: InsuranceService
   ) { super(); }
 
   ngOnInit(): void {
@@ -30,6 +34,7 @@ export class DentalInsuranceComponent extends Base implements OnInit {
       filter(p => !!p),
       switchMap(p => {
         this.addressId = p.clinicId;
+        this.getClinicCodes();
         return forkJoin([
           this.clinicService.getClinic(p.clinicId).pipe(map(d => d.data), take(1)),
           this.patientService.getPatient(p.patientId).pipe(map(d => d.data), take(1))
@@ -44,6 +49,50 @@ export class DentalInsuranceComponent extends Base implements OnInit {
 
   backToPatients(): void {
     this.router.navigate([`agent/clinics/${this.addressId}/patients`]);
+  }
+
+  private getClinicCodes(): void {
+    forkJoin([
+      this.clinicService.getSelectedPracticeCodes(this.addressId).pipe(map(r => r.data), take(1)),
+      this.insuranceService.getPracticeCodes().pipe(take(1))
+    ]).pipe(
+      map(res => this.mapToCodes(res)),
+      takeUntil(this.unsubscribe$)
+    )
+      .subscribe(codes => {
+        this.savedCodes = codes;
+        console.log(codes);
+      });
+  }
+
+  private mapToCodes([clinicCodes, insuranceCodes]): any {
+    const savedCodes = this.newSavedCodes();
+    savedCodes.label = 'Categories';
+    savedCodes.key = 'categories';
+    savedCodes.breakDownKeys = [];
+    clinicCodes.forEach(group => {
+      const groupId = group.groupId;
+      savedCodes.breakDownKeys.push(groupId);
+      const breakDowns = {};
+      group.codeIds.forEach(id => breakDowns[id] = insuranceCodes.breakDowns[groupId].breakDowns[id]);
+      savedCodes.breakDowns[groupId] = {
+        key: groupId,
+        label: insuranceCodes.breakDowns[groupId].label,
+        breakDownKeys: group.codeIds,
+        breakDowns,
+      };
+    });
+
+    return savedCodes;
+  }
+
+  private newSavedCodes(): DentalBreakDowns {
+    return {
+      key: '',
+      label: '',
+      breakDownKeys: [],
+      breakDowns: {}
+    };
   }
 
 }
