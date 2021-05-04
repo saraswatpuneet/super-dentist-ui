ced03a272a6b5a9ed3f214efe8f654a1793c2ba7
DTwoPaw <xthecounsel@gmail.com>
2021-05-03 21:42:00 -0500

    Convert code inputs to a module

--- src/app/agent/shared/agent-input/agent-input.module.ts
+++ src/app/agent/shared/agent-input/agent-input.module.ts
@@ -15,8 +15,9 @@ import { NgxMaskModule } from 'ngx-mask';
 import { AgentInputComponent } from './agent-input.component';
 import { CodeCategoryComponent } from '../code-category/code-category.component';
 import { PatientDetails2Component } from '../patient-details/patient-details.component';
-import { ToothHistoryComponent } from '../tooth-history/tooth-history.component';
 import { PatientStatusPillModule } from 'src/app/shared/patient-status-pill/patient-status-pill.module';
+import { ToothHistoryComponent } from '../tooth-history/tooth-history.component';
+import { CodeInputsModule } from '../code-inputs/code-inputs.module';
 
 @NgModule({
   declarations: [AgentInputComponent, CodeCategoryComponent, PatientDetails2Component, ToothHistoryComponent],
@@ -28,6 +29,7 @@ import { PatientStatusPillModule } from 'src/app/shared/patient-status-pill/pati
     MatCardModule,
     MatDatepickerModule,
     MatIconModule,
+    CodeInputsModule,
     MatProgressBarModule,
     MatRadioModule,
     MatSelectModule,
--- src/app/agent/shared/code-inputs/code-inputs.module.ts
+++ src/app/agent/shared/code-inputs/code-inputs.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatSelectModule } from '@angular/material/select';
+import { MatRadioModule } from '@angular/material/radio';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
-
+import { CodeInputsComponent } from './code-inputs.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [CodeInputsComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatRadioModule,
+    MatSelectModule,
+  ],
+  exports: [CodeInputsComponent]
 })
 export class CodeInputsModule { }
