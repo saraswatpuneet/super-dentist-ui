03fa0f8fbc0bcc5b9520e4ac4028ade5a0f065a2
DTwoPaw <xthecounsel@gmail.com>
2021-05-05 21:46:32 -0500

    Code inputs shared codes works for medical insruance

--- src/app/agent/shared/code-inputs/code-inputs.component.ts
+++ src/app/agent/shared/code-inputs/code-inputs.component.ts
@@ -21,39 +21,67 @@ export class CodeInputsComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    setTimeout(() => console.log(this.groupModel), 1500);
   }
 
   checkOtherOptions($event, selectThatChanged: string): void {
     // removes key from all codes
     this.codeList.forEach(key => {
-      this.groups.controls.forEach((group: FormGroup) => {
-        const control = group.controls.codes.get(key);
-        if (control) {
-          const cas = control.get('sharedCodes');
-          if (cas.value) {
-            const index = cas.value.indexOf(selectThatChanged);
+      this.groupModel.forEach((group) => {
+        if (group.codes[key]) {
+          if (group.codes[key].sharedCodes) {
+            const index = group.codes[key].sharedCodes.indexOf(selectThatChanged);
             if (index > -1) {
-              cas.value.splice(index, 1);
-              cas.setValue([...cas.value]);
+              group.codes[key].sharedCodes.splice(index, 1);
+              group.codes[key].sharedCodes = [...group.codes[key].sharedCodes];
             }
           }
         }
       });
     });
 
+    // removes key from all codes
+    // this.codeList.forEach(key => {
+    //   this.groups.controls.forEach((group: FormGroup) => {
+    //     const control = group.controls.codes.get(key);
+    //     if (control) {
+    //       const cas = control.get('sharedCodes');
+    //       if (cas.value) {
+    //         const index = cas.value.indexOf(selectThatChanged);
+    //         if (index > -1) {
+    //           cas.value.splice(index, 1);
+    //           cas.setValue([...cas.value]);
+    //         }
+    //       }
+    //     }
+    //   });
+    // });
+
     //  Adds the key to other codes that were selected
     $event.value.forEach(key => {
-      this.groups.controls.forEach((group: FormGroup) => {
-        const control = group.controls.codes.get(key);
-        if (control) {
-          const cas = control.get('sharedCodes');
-          if (cas.value) {
-            cas.setValue([selectThatChanged, ...cas.value]);
+      this.groupModel.forEach((group) => {
+        if (group.codes[key]) {
+          if (group.codes[key].sharedCodes) {
+            group.codes[key].sharedCodes = [selectThatChanged, ...group.codes[key].sharedCodes];
           } else {
-            cas.setValue([selectThatChanged]);
+            group.codes[key].sharedCodes = [selectThatChanged];
           }
         }
       });
     });
+    console.log(this.groupModel);
+    // $event.value.forEach(key => {
+    //   this.groups.controls.forEach((group: FormGroup) => {
+    //     const control = group.controls.codes.get(key);
+    //     if (control) {
+    //       const cas = control.get('sharedCodes');
+    //       if (cas.value) {
+    //         cas.setValue([selectThatChanged, ...cas.value]);
+    //       } else {
+    //         cas.setValue([selectThatChanged]);
+    //       }
+    //     }
+    //   });
+    // });
   }
 }
