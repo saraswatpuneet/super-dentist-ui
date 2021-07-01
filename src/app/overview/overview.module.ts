167e5c824a958e3ffac6579374055fabdd705f2f
DTwoPaw <xthecounsel@gmail.com>
2021-07-01 14:17:08 -0500

    Overview card placeholders

--- src/app/overview/overview.module.ts
+++ src/app/overview/overview.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatCardModule } from '@angular/material/card';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { HighchartsChartModule } from 'highcharts-angular';
 
 import { OverviewRoutingModule } from './overview-routing.module';
 import { OverviewComponent } from './overview.component';
-
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [
@@ -11,6 +16,12 @@ import { OverviewComponent } from './overview.component';
   ],
   imports: [
     CommonModule,
+    FlexLayoutModule,
+    HighchartsChartModule,
+    MatCardModule,
+    MatSelectModule,
+    MatInputModule,
+    MatButtonModule,
     OverviewRoutingModule
   ]
 })
