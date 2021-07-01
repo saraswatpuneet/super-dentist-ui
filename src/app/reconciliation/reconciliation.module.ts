92fd84eb63cc4c71f7878134dcf52114a6090ad3
DTwoPaw <xthecounsel@gmail.com>
2021-07-01 14:22:39 -0500

    Placeholders and modules for remaining components

--- src/app/reconciliation/reconciliation.module.ts
+++ src/app/reconciliation/reconciliation.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { HighchartsChartModule } from 'highcharts-angular';
+import { MatCardModule } from '@angular/material/card';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 import { ReconciliationRoutingModule } from './reconciliation-routing.module';
 import { ReconciliationComponent } from './reconciliation.component';
-
 
 @NgModule({
   declarations: [
@@ -11,6 +16,12 @@ import { ReconciliationComponent } from './reconciliation.component';
   ],
   imports: [
     CommonModule,
+    FlexLayoutModule,
+    HighchartsChartModule,
+    MatCardModule,
+    MatSelectModule,
+    MatInputModule,
+    MatButtonModule,
     ReconciliationRoutingModule
   ]
 })
