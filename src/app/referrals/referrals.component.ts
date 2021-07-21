4b8993ccd2a38c0b54df641ac78cb79f15bb4563
DTwoPaw <xthecounsel@gmail.com>
2021-07-21 16:26:31 -0500

    Filter patients on referrals

--- src/app/referrals/referrals.component.ts
+++ src/app/referrals/referrals.component.ts
@@ -22,7 +22,9 @@ import { referralsAnimations } from './referrals.animations';
 export class ReferralsComponent extends Base implements OnInit {
   tabCounts = [undefined, undefined, undefined, undefined];
   files: any;
+  patientNameSearch = '';
   clinicReferrals: any[] = [];
+  filteredClinicReferrals: any[] = [];
   referralColumns: string[] = ['dateReferred', 'patientName', 'referringClinic', 'actions'];
   messageToSend = '';
   messages: Message[];
@@ -87,15 +89,32 @@ export class ReferralsComponent extends Base implements OnInit {
       saveAs(new Blob([res]), 'patientDocuments.zip'));
   }
 
+  filterPatientList($event): void {
+    const clinicReferrals = JSON.parse(JSON.stringify(this.clinicReferrals));
+    if ($event) {
+
+      for (let x = 0, l = clinicReferrals.length; x < l; x++) {
+        const referrals = clinicReferrals[x].referrals;
+        for (let y = 0, l2 = referrals.length; y < l2; y++) {
+          clinicReferrals[x].referrals[y] = referrals[y].filter(r =>
+            `${r.patientFirstName} ${r.patientLastName}`.toLowerCase().includes($event.toLowerCase())
+          );
+        }
+      }
+      this.filteredClinicReferrals = clinicReferrals;
+    } else {
+      this.filteredClinicReferrals = clinicReferrals;
+    }
+
+    if (this.clinicType === 'specialist') {
+      this.calcTabCounts(4);
+    } else {
+      this.calcTabCounts(2);
+    }
+  }
+
   getFilteredSpecialistReferrals(referrals: Referral[], tabIndex: number): any {
-    let validStates = [this.sortedStatuses[tabIndex]];
-    // if (tabIndex === 0) {
-    //   validStates = [this.sortedStatuses[0]];
-    // } else if (tabIndex === 1) {
-    //   validStates = [this.sortedStatuses[1]];
-    // } else {
-    //   validStates = [this.sortedStatuses[2], this.sortedStatuses[4]];
-    // }
+    const validStates = [this.sortedStatuses[tabIndex]];
 
     return referrals.filter(r => {
       if (!r.status) {
@@ -121,9 +140,9 @@ export class ReferralsComponent extends Base implements OnInit {
   getFilteredDentistReferrals(referrals: Referral[], tabIndex: number): any {
     let validStates = [];
     if (tabIndex === 0) {
-      validStates = [this.sortedStatuses[0], this.sortedStatuses[1], this.sortedStatuses[2]];
+      validStates = [this.sortedStatuses[0], this.sortedStatuses[1], this.sortedStatuses[2], this.sortedStatuses[3]];
     } else {
-      validStates = [this.sortedStatuses[4]];
+      validStates = [this.sortedStatuses[5]];
     }
 
     return referrals.filter(r => {
@@ -140,11 +159,15 @@ export class ReferralsComponent extends Base implements OnInit {
   }
 
   markStatusDentist(referralIndex: number, status: ClinicStatus): void {
-    const ref = this.clinicReferrals[0].referrals[0].splice(referralIndex, 1)[0];
+    const ref = this.filteredClinicReferrals[0].referrals[0].splice(referralIndex, 1)[0];
     ref.status = { gdStatus: status, spStatus: closed };
+    this.filteredClinicReferrals[0].referrals[1].push(ref);
+    this.filteredClinicReferrals = JSON.parse(JSON.stringify(this.filteredClinicReferrals));
+
+    const indexToSplice = this.clinicReferrals[0].referrals[0].findIndex(r => r.referralId === ref.referralId);
+    this.clinicReferrals[0].referrals[0].splice(indexToSplice, 1);
     this.clinicReferrals[0].referrals[1].push(ref);
 
-    this.clinicReferrals = JSON.parse(JSON.stringify(this.clinicReferrals));
     this.calcTabCounts(2);
     this.referralService.updateStatus(ref.referralId, { gdStatus: status, spStatus: status })
       .pipe(take(1))
@@ -158,20 +181,23 @@ export class ReferralsComponent extends Base implements OnInit {
   }
 
   updateStatus2(clinicIndex: number, referralIndex: number, newTabIndex: number, status: ClinicStatus): void {
-    let ref = this.clinicReferrals[clinicIndex].referrals[this.tabIndex][referralIndex];
+    let ref = this.filteredClinicReferrals[clinicIndex].referrals[this.tabIndex][referralIndex];
     if (ref.status.gdStatus === status) {
       return;
     }
 
-    ref = this.clinicReferrals[clinicIndex].referrals[this.tabIndex].splice(referralIndex, 1)[0];
-
+    ref = this.filteredClinicReferrals[clinicIndex].referrals[this.tabIndex].splice(referralIndex, 1)[0];
     ref.status = { gdStatus: status, spStatus: status };
+    this.filteredClinicReferrals[clinicIndex].referrals[newTabIndex].push(ref);
+    this.filteredClinicReferrals = JSON.parse(JSON.stringify(this.filteredClinicReferrals));
 
+    // I have to find the original index and update clinicReferrals or filtering won't work.
+    // this.clinicReferrals
+    const indexToSplice = this.clinicReferrals[clinicIndex].referrals[this.tabIndex].findIndex(r => r.referralId === ref.referralId);
+    this.clinicReferrals[clinicIndex].referrals[this.tabIndex].splice(indexToSplice, 1);
     this.clinicReferrals[clinicIndex].referrals[newTabIndex].push(ref);
 
-    // Update tables
-    this.clinicReferrals = JSON.parse(JSON.stringify(this.clinicReferrals));
-    this.calcTabCounts(4);
+    this.filterPatientList(this.patientNameSearch);
     this.referralService.updateStatus(ref.referralId, { gdStatus: status, spStatus: status })
       .pipe(take(1))
       .subscribe();
@@ -199,7 +225,7 @@ export class ReferralsComponent extends Base implements OnInit {
   private calcTabCounts(arraySize: number): void {
     this.tabCounts = [...Array(arraySize)].map((_, i) => {
       let count = 0;
-      this.clinicReferrals.forEach(c => {
+      this.filteredClinicReferrals.forEach(c => {
         count += c.referrals[i].length;
       });
 
@@ -222,7 +248,7 @@ export class ReferralsComponent extends Base implements OnInit {
         this.getFilteredDentistReferrals(res as Referral[], 1)
       ];
       this.clinicReferrals = [{ clinicName: this.myClinics[0].name, clinicAddress: this.myClinics[0].address, referrals }];
-      this.calcTabCounts(2);
+      this.filterPatientList('');
     });
   }
 
@@ -251,7 +277,7 @@ export class ReferralsComponent extends Base implements OnInit {
           referrals
         });
       }
-      this.calcTabCounts(4);
+      this.filterPatientList('');
     });
   }
 
